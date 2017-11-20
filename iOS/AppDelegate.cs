b46e821b80a6f3e4ea8737404e0d22d0c9e38b9f
Stivixd123 <estebangonzalez20@live.com>
2017-11-20 02:52:46 -0500

    more updates

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -12,6 +12,7 @@ namespace HLA4.iOS
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
             global::Xamarin.Forms.Forms.Init();
 
             LoadApplication(new App());
